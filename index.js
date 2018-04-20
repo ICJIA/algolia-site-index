d295c60cabc6f02d3985396f9fc065b78ea12ef7
Chris Schweda <cschweda@gmail.com>
2018-04-20 09:34:35 -0500

    Fix for title in document meta

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 let _ = require("lodash");
 let jsonfile = require("jsonfile");
 let wtj = require("website-to-json");
@@ -8,15 +9,28 @@ let parser = require("xml2json");
 let algoliaIndex = [];
 let algoliasearch = require("algoliasearch");
 let crypto = require("crypto");
+let logger = require("./utils/logger").createLogger();
 
 // For local testing:
 // let path = require("path");
 // let xmlPath = path.join(__dirname, "sitemap.xml");
 // let res = fs.readFileSync(xmlPath);
 
+// Get current json of Algolia objects
+let config, sitemap, index;
+try {
+  config = jsonfile.readFileSync("./config.json");
+  logger.info("config.json fetched");
+  appIndex = config["ifvcc"].index || "default";
+  sitemap = config["ifvcc"].sitemap || "default";
+} catch (e) {
+  logger.error("config.json does not exist");
+  process.exit(1);
+}
+
 // Get Sitemap from URL
 let request = require("sync-request");
-let res = request("GET", process.env.SITEMAP_URL);
+let res = request("GET", sitemap);
 let xml = res.getBody().toString("utf8");
 let myJson = parser.toJson(xml);
 let urls = JSON.parse(myJson)["urlset"].url;
@@ -24,10 +38,11 @@ let urls = JSON.parse(myJson)["urlset"].url;
 // Get current json of Algolia objects
 let algoliaOldContent;
 try {
-  algoliaOldContent = jsonfile.readFileSync("./algolia.search.json");
-  console.log("Old content objects fetched.");
+  algoliaOldContent = jsonfile.readFileSync(`./${appIndex}.json`);
+  logger.info(`${appIndex}.json fetched`);
 } catch (e) {
-  console.log("File does not exist");
+  logger.error(`${appIndex}.json does not exist`);
+  process.exit(1);
 }
 
 let arrayOfUrls = urls.map(url => {
@@ -47,13 +62,17 @@ function get(url) {
     fields: ["data"],
     parse: function($) {
       return {
-        title: $("meta[name='title' i]").attr("content") || "title undefined",
-        description: $("meta[name='description' i]").attr("content"),
-        keywords: $("meta[name='keywords' i]").attr("content") || "undefined",
+        title: $("title").text() || "title undefined",
+        description:
+          $("meta[name='description' i]").attr("content") ||
+          "description undefined",
+        keywords:
+          $("meta[name='keywords' i]").attr("content") || "keywords undefined",
         url: url,
-        body: $("#page-content")
-          .text()
-          .substring(0, 2500),
+        body:
+          $("#page-content")
+            .text()
+            .substring(0, 2500) || "page content undefined",
         objectID: objectID
       };
     }
@@ -66,6 +85,7 @@ arrayOfUrls.map(async function(url) {
     algoliaIndex.push(res.data);
     if (num === 0) {
       // Finished generating
+      logger.info(`Finished querying URLs`);
       setSearchObjects(algoliaIndex);
     }
   });
@@ -78,11 +98,11 @@ function setSearchObjects(objects) {
     process.env.ALGOLIA_ADMIN_API_KEY
   );
 
-  let index = client.initIndex(process.env.ALGOLIA_INDEX);
+  let index = client.initIndex(`${appIndex}`);
   index.addObjects(objects, function(err, algoliaNewContent) {
     // save current pages
-    jsonfile.writeFileSync("./algolia.search.json", algoliaNewContent);
-
+    jsonfile.writeFileSync(`${appIndex}.json`, algoliaNewContent);
+    logger.info(`${appIndex}.json written`);
     // detect deletions
     let oldArr = algoliaOldContent["objectIDs"];
     let newArr = algoliaNewContent["objectIDs"];
@@ -92,8 +112,12 @@ function setSearchObjects(objects) {
       console.log("Delete: ", objectsToDelete);
       index.deleteObjects(objectsToDelete, function(err, res) {
         if (err) throw err;
-        console.log("Successfully deleted: ", res);
+        logger.info(`Successfully deleted: ${res}`);
       });
+    } else {
+      logger.info("No deletions detected");
     }
+
+    logger.info("Algolia search index updated.");
   });
 }
